a6c7a52fbecd744eefb549dc4d93989411a3b55b
Karsten Thoms <karsten.thoms@gmail.com>
2024-10-29 00:57:15 +0100

    [#27] Refactor to JUnit 5

--- spin/core/src/test/java/org/operaton/spin/impl/test/Script.java
+++ spin/core/src/test/java/org/operaton/spin/impl/test/Script.java
@@ -16,15 +16,13 @@
  */
 package org.operaton.spin.impl.test;
 
-import org.junit.rules.TestRule;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Method annotation to signal a script test for the {@link ScriptRule} {@link TestRule}.
+ * Method annotation to signal a script test for the {@link ScriptRule} {@link org.junit.jupiter.api.extension.Extension}.
  * The name of the script is optional and if omitted the package name is used as directory
  * and the ClassName.MethodName.ScriptExtension as filename. Additionally a list of
  * variables can be defined and the execute flag decides whether the script is execute
